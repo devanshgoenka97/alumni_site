c6b540ccfcee79e7ae13fd3a0760831f8b5abda7
Devansh Goenka <devanshgoenka97@gmail.com>
2017-08-24 02:07:27 +0530

    Significant changes

--- js/custom.js
+++ js/custom.js
@@ -32,3 +32,25 @@ $("#searchalumni").click(function(){
 $("#alumniregs").click(function(){
      fetchSection('alumniregs');
 });
+
+var scrollpos = window.scrollY;
+var header = document.getElementById("navbar");
+var windowheight = window.innerHeight;
+var headerheight = document.getElementById("header").offsetHeight;
+
+function add_class_on_scroll() {
+  header.classList.add("affix");
+}
+
+function remove_class_on_scroll() {
+  header.classList.remove("affix");
+}
+
+window.addEventListener('scroll', function() {
+  scrollpos = window.scrollY;
+  if (scrollpos > 67)  {
+    add_class_on_scroll();
+  } else {
+    remove_class_on_scroll();
+  }
+});
