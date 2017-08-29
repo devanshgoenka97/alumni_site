4f1eb7484ad7a9514295116ccff970718d17d75e
Devansh Goenka <devanshgoenka97@gmail.com>
2017-08-29 15:20:52 +0530

    minor fixes and full responsive

--- js/custom.js
+++ js/custom.js
@@ -67,7 +67,7 @@ function remove_class_on_scroll() {
 
 window.addEventListener('scroll', function() {
   scrollpos = window.scrollY;
-  if (scrollpos > 66)  {
+  if (scrollpos > 70)  {
     add_class_on_scroll();
   } else {
     remove_class_on_scroll();
